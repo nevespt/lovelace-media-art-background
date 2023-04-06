d3c3816854d82252e61561705f98a9f4f6e23112
gompen <gompen@gmail.com>
2023-04-06 18:10:54 +0200

    Update media-art-background.js

--- media-art-background.js
+++ media-art-background.js
@@ -88,7 +88,8 @@ let root = document.querySelector("home-assistant");
 root = root.shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("ha-drawer partial-panel-resolver ha-panel-lovelace").shadowRoot.querySelector("hui-root");
 
 // get constant elements from HA root element
-const appLayout = root.shadowRoot.querySelector("ha-app-layout");
+//const appLayout = root.shadowRoot.querySelector("ha-app-layout");
+const appLayout = root.shadowRoot.querySelector("hui-view");
 const lovelace = root.lovelace;
 
 // create container element, set style and append to container
